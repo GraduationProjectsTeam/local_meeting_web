5e0b9a5835331af913bfd3b33bddded344970246
Tigg <ignatovichanastasiia@gmail.com>
2022-01-11 04:07:32 +0300

    Add new project Local Meeting Web

---
+++ src/main/java/ru/geekbrains/local_meeting_web/LocalMeetingWebApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.local_meeting_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalMeetingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalMeetingWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/local_meeting_web/LocalMeetingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.local_meeting_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalMeetingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
